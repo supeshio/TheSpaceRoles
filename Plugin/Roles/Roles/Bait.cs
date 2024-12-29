bb5daa08f25818233e4f338ffdfcd5dc2f5490b1
supeshio <supeshio314@gmail.com>
2024-12-29 23:22:43 +0900

    Update Bait.cs

--- Plugin/Roles/Roles/Bait.cs
+++ Plugin/Roles/Roles/Bait.cs
@@ -67,7 +67,7 @@ namespace TheSpaceRoles
                             vent.myRend.material.SetFloat("_Outline", 0);
                         }
                     }
-                    DestroyableSingleton<VentCleaningMinigame>.Instance.
+                    //DestroyableSingleton<VentCleaningMinigame>.Instance.
                 }
 
             }
